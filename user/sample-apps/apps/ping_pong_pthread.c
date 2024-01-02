75b9e241d9aff834103878a0b7b426ae8fa06d7f
wfn <fangnuowu@gmail.com>
2024-01-02 18:07:06 +0800

    add pingpong-pthread version

---
+++ user/sample-apps/apps/ping_pong_pthread.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <pthread.h>
+
+#include <chcore/syscall.h>
+#include <chcore/launcher.h>
+
+#define LEADER   0
+#define FOLLOWER 1
+#define TRUE     1
+#define FALSE    0
+
+const int sleep_every_step = TRUE;
+
+struct shared_data {
+        int turn;
+        int counter;
+};
+
+volatile struct shared_data *region;
+
+void *count(void *arg)
+{
+        printf("arg = %p\n", arg);
+        int myturn = (int)(long)arg;
+        printf("myturn = %d\n", myturn);
+        for (;;) {
+                /* wait for my turn */
+                while (region->turn != myturn)
+                        ;
+
+                /* output and add 1 */
+                printf("%d: [%d]\n", myturn, region->counter++);
+
+                /* give it to the other process */
+                region->turn ^= 1;
+
+                if (sleep_every_step) {
+                        sleep(1);
+                }
+        }
+        return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+        // pthread_attr_t attr;
+        pthread_t tid;
+        
+        /* create shared memory */
+        region = (volatile struct shared_data *)malloc(
+                sizeof(struct shared_data));
+        printf("shared mem created\n");
+        assert(region != NULL);
+
+        /* initialize the shared structure */
+        region->turn = 0; // leader
+        region->counter = 1;
+
+        /* spawn the follower process */
+        pthread_create(&tid, NULL, count, (void *)(long)FOLLOWER);
+
+        /* start counting */
+        count((void *)(long)LEADER);
+        
+        return 0;
+}
