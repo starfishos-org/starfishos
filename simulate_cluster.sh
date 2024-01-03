1f906e56fcc903254844c38a85fe9b0f02a8927e
wfn <fangnuowu@gmail.com>
2024-01-03 16:39:23 +0800

    scripts: add a script to init two window to test

---
+++ simulate_cluster.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# use ./simulayed_cluster build to build and start the program
+if [ "$1" == "build" ]; then
+	./chbuild build
+fi
+
+rm -rf /dev/shm/ivshmem
+
+## Create a Tmux session "mywork" in a window "window0" started in the background.
+tmux new -d -s mywork -n window0
+
+## Split the window to 4 panes.
+tmux split-window -h -t mywork:window0
+# tmux split-window -v -t mywork:window0.0
+# tmux split-window -v -t mywork:window0.1
+
+## Run the ROS programs sequentially.
+tmux send -t mywork:window0.0 "./build/simulate.sh" ENTER
+sleep 3
+tmux send -t mywork:window0.1 "./build/simulate.sh" ENTER
+# tmux send -t mywork:window0.2 "source setup.bash && roslaunch prog3 p3.launch" ENTER
+# tmux send -t mywork:window0.3 "source setup.bash && roslaunch prog4 p4.launch" ENTER
+
+## Attach the Tmux session to the front.
+tmux a -t mywork
