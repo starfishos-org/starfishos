2b5cf27335563c3f688d085d4f945b234992b0fe
wfn <fangnuowu@gmail.com>
2024-02-19 11:15:40 +0000

    scripts: update

--- scripts/qemu/emulate.tpl.sh
+++ scripts/qemu/emulate.tpl.sh
@@ -6,6 +6,8 @@ basedir=$(dirname "$0")
 # basedir should be /build directory
 nvm_backend_file="/tmp/nvm-file-$USER"
 
+ivshmem_dev="/dev/shm/ivshmem-$USER"
+
 port=$(shuf -i 30000-40000 -n 1)
 #while true; do
 #	port=\$(shuf -i 30000-40000 -n 1)
