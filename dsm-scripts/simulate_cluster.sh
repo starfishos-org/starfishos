0bfb55bf43430838602094d964fba8fb01d3c8d8
yan <yanjingsheng@sjtu.edu.cn>
2024-04-22 15:20:40 +0800

    tmux conf

--- dsm-scripts/simulate_cluster.sh
+++ dsm-scripts/simulate_cluster.sh
@@ -16,9 +16,9 @@ tmux split-window -v -t mywork:window0
 # tmux split-window -v -t mywork:window0.1
 
 ## Run the ROS programs sequentially.
-tmux send -t mywork:window0.1 "./build/simulate.sh > exec_log | tee exec_log1" ENTER
+tmux send -t mywork:window0.0 "./build/simulate.sh > exec_log | tee exec_log1" ENTER
 sleep 3
-tmux send -t mywork:window0.2 "./build/simulate.sh > exec_log | tee exec_log2" ENTER
+tmux send -t mywork:window0.1 "./build/simulate.sh > exec_log | tee exec_log2" ENTER
 # tmux send -t mywork:window0.2 "source setup.bash && roslaunch prog3 p3.launch" ENTER
 # tmux send -t mywork:window0.3 "source setup.bash && roslaunch prog4 p4.launch" ENTER
 
