b63014dc2648b8eaf6d534de3cc4c431c890f6ba
Jingsheng Yan <yanjingsheng@sjtu.edu.cn>
2025-01-10 18:17:47 +0800

    fix bug

--- dsm-scripts/simulate_4clusters.sh
+++ dsm-scripts/simulate_4clusters.sh
@@ -24,7 +24,7 @@ tmux send -t $session_name:$window_name.0 "./build/simulate.sh 0 > exec_log | te
 sleep 3
 tmux send -t $session_name:$window_name.1 "./build/simulate.sh 1 > exec_log | tee exec_log2" ENTER
 tmux send -t $session_name:$window_name.2 "./build/simulate.sh 2 > exec_log | tee exec_log3" ENTER
-tmux send -t $session_name:$window_name.3 "./bui1ld/simulate.sh 3 > exec_log | tee exec_log4" ENTER
+tmux send -t $session_name:$window_name.3 "./build/simulate.sh 3 > exec_log | tee exec_log4" ENTER
 
 tmux select-pane -t $session_name:$window_name.0
 
