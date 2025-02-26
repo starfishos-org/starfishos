508c795abd2f0f27a79a7e1f6ce15c474dafbd08
wfn <fangnuowu@gmail.com>
2025-02-26 21:24:34 +0800

    fix: error dependency

--- user/libraries/lkl-libs/liblkl.cmake
+++ user/libraries/lkl-libs/liblkl.cmake
@@ -16,13 +16,5 @@ function(build_liblkl)
         COMMAND make CC=${MUSL_CC} EXTRA_CFLAGS='-U__linux__ -D__CHCORE__' -C tools/lkl -j
         COMMENT "Build lkl finished"
         DEPENDS)
-
-    add_custom_command(
-        TARGET lkl
-        POST_BUILD
-        COMMAND cp ${LKL_LKL_DIR}/liblkl.a ${build_ramdisk_dir}/liblkl.a
-        COMMENT "Copying llama.cpp files to ramdisk directory"
-        DEPENDS lkl)
     add_dependencies(ramdisk lkl)
-    add_dependencies(lkl_virtio_blk lkl)
 endfunction()
